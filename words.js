160eee53969183fb8b7d55cdc2a8205798f4bb97
Cavin Mozarmi <Cavin@dhcp-18-189-70-50.dyn.mit.edu>
2017-05-11 13:45:12 -0400

    took out sours

--- words.js
+++ words.js
@@ -1,6 +1,6 @@
 var WORDS = ["rum", "lime", "mint", "sugar", "refreshing", "tequila", "triple sec", "salt", "cachaca", "gin",
   "simple syrup", "lemon", "cola", "pineapple", "coconut milk", "coconut", "ginger beer", "campari", "vermouth",
-  "club soda", "grenadine", "orange juice", "fruity", "sparkling wine", "bourbon", "sours", "citus", "vodka",
+  "club soda", "grenadine", "orange juice", "fruity", "sparkling wine", "bourbon", "citus", "vodka",
   "cointreau", "cranberry juice", "almond syrup", "orange curaçao", "irish whiskey", "brown sugar syrup",
   "coffee", "whipped cream", "cognac", "tomato juice", "tabasco sauce", "horseradish", "celery salt",
   "pepper", "paprika", "worcestershire sauce", "bitters", "cherry", "rye", "whiskey", "orange", "kahlua",
