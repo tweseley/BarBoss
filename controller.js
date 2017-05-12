31e2afb269f6e14601eaca982f3351fac4f699f3
Cavin Mozarmi <Cavin@dhcp-18-189-62-125.dyn.mit.edu>
2017-05-11 23:52:35 -0400

    window resizing and deleted comments in controller

--- controller.js
+++ controller.js
@@ -1,50 +1,21 @@
 var CONTROLLER = (function(document) {
 
-  // The <input type="text"> containing the search term.
   var searchTermInput;
-
-  // The <ul> containing the possible autocompletions.
   var resultsList;
-
-  // Function to be called on every keyup event
   var autocompleteListener;
 
-  /**
-   * Registers a listener function to be called after each keypress.
-   * The listener will be passed a single String word as an argument
-   * when invoked.
-   *
-   * @param {Function} listener - The function to be called. Function
-   *   must accept a single String as an argument.
-   */
   function registerAutocompleteListener(listener) {
     autocompleteListener = listener;
   }
 
-  /**
-   * Appends a String value to the end of the search results list.
-   *
-   * @param {String} value - The text to append to the search list.
-   */
   function appendToSearchResults(value) {
     resultsList.appendChild(createListElement(value, onResultClick));
   }
 
-  /**
-   * Removes all entries existing in the search results.
-   */
   function clearSearchResults() {
     resultsList.innerHTML = '';
   }
 
-  /**
-   * Create an <li> that contains the given value and executes the given
-   * handler when it is clicked.
-   *
-   * @param {String} value - The text to place inside the <li>.
-   * @param {Function} onClick - The event handler to execute when the <li> is
-   *  clicked.
-   */
   function createListElement(value, onClick) {
     var li = document.createElement('li');
     li.innerHTML = value;
@@ -52,23 +23,13 @@ var CONTROLLER = (function(document) {
     return li;
   }
 
-  /**
-   * Handle the click of an autocompletion.
-   *
-   * @param {Object} event - The click event.
-   */
   function onResultClick(event) {
     searchTermInput.value = event.target.innerHTML;
     clearSearchResults();
   }
 
-  /**
-   * Perform autocompletion each time a key is typed.
-   */
   function onKeyUp() {
     clearSearchResults();
-    // If there's a search term and a registered listener,
-    // invoke the listener with the search term.
     var searchTerm = searchTermInput.value
     if (searchTerm.length > 0 && autocompleteListener != undefined) {
       autocompleteListener(searchTerm);
@@ -81,7 +42,6 @@ var CONTROLLER = (function(document) {
     searchTermInput.addEventListener('keyup', onKeyUp, false);
   });
 
-  /** Public exports for CONTROLLER module **/
   return {
     "registerAutocompleteListener" : registerAutocompleteListener,
     "clearSearchResults" : clearSearchResults,
